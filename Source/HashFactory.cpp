00560cb9956c179cf47c698e608a68fe204279dd
Shishir Bhat <shishir993@users.noreply.github.com>
2017-10-12 22:03:59 -0700

    Fix #25 Enable hashing of large files
    Hash 16MB at a time, in a loop

--- Source/HashFactory.cpp
+++ Source/HashFactory.cpp
@@ -13,7 +13,7 @@
 #define MBYTES  (1024 * 1024)
 
 static HRESULT _HashFileOneShot(_In_ HCRYPTPROV hCrypt, _In_ HANDLE hFile, _In_ DWORD cbFile, _Out_bytecap_c_(HASHLEN_SHA1) PBYTE pbHash);
-static HRESULT _HashFilePieceMeal(_In_ HCRYPTPROV hCrypt, _In_ HANDLE hFile, _In_ INT64 cbFile, _Out_bytecap_c_(HASHLEN_SHA1) PBYTE pbHash);
+static HRESULT _HashFilePieceMeal(_In_ HCRYPTPROV hCrypt, _In_ HANDLE hFile, _In_ UINT64 cbFile, _Out_bytecap_c_(HASHLEN_SHA1) PBYTE pbHash);
 
 
 HRESULT HashFactoryInit(_Out_ HCRYPTPROV *phCrypt)
@@ -62,18 +62,21 @@ HRESULT CalculateSHA1(_In_ HCRYPTPROV hCrypt, _In_ HANDLE hFile, _Out_bytecap_c_
     
     if ((200 * MBYTES) <= fileSize.QuadPart)
     {
-        hr = HRESULT_FROM_WIN32(ERROR_NOT_SUPPORTED);
-        logerr(L"File too big");
-        goto fend;
+        hr = _HashFilePieceMeal(hCrypt, hFile, fileSize.QuadPart, pbHash);
+    }
+    else
+    {
+        hr = _HashFileOneShot(hCrypt, hFile, fileSize.LowPart, pbHash);
     }
 
-    hr = _HashFileOneShot(hCrypt, hFile, fileSize.LowPart, pbHash);
-
+    if (SUCCEEDED(hr))
+    {
 #ifdef _DEBUG
-    CHAR szHash[STRLEN_SHA1];
-    HashValueToString(pbHash, szHash);
-    logdbg(L"Hash string = [%S]", szHash);
+        CHAR szHash[STRLEN_SHA1];
+        HashValueToString(pbHash, szHash);
+        logdbg(L"Hash string = [%S]", szHash);
 #endif
+    }
 
 fend:
     return hr;
@@ -146,7 +149,69 @@ fend:
     return hr;
 }
 
-HRESULT _HashFilePieceMeal(_In_ HCRYPTPROV hCrypt, _In_ HANDLE hFile, _In_ INT64 cbFile, _Out_bytecap_c_(HASHLEN_SHA1) PBYTE pbHash)
+HRESULT _HashFilePieceMeal(_In_ HCRYPTPROV hCrypt, _In_ HANDLE hFile, _In_ UINT64 cbFile, _Out_bytecap_c_(HASHLEN_SHA1) PBYTE pbHash)
 {
-    return E_NOTIMPL;
+    HRESULT hr = S_OK;
+    void* pBuffer = NULL;
+    UINT64 cbRemaining = cbFile;
+    const DWORD cbBuf = 16 * MBYTES;
+
+    HCRYPTHASH hHash = NULL;
+    DWORD dwHashSize = HASHLEN_SHA1;
+
+    hr = CHL_MmAlloc(&pBuffer, cbBuf, NULL);
+    if (FAILED(hr))
+    {
+        goto fend;
+    }
+
+    if (!CryptCreateHash(hCrypt, CALG_SHA1, NULL, 0, &hHash))
+    {
+        hr = HRESULT_FROM_WIN32(GetLastError());
+        logerr(L"CryptCreateHash() failed");
+        goto fend;
+    }
+
+    while (0 < cbRemaining)
+    {
+        DWORD cbRead = 0;
+        DWORD cbToRead = (DWORD)(min(cbRemaining, cbBuf));
+        if (!ReadFile(hFile, pBuffer, cbToRead, &cbRead, NULL))
+        {
+            hr = HRESULT_FROM_WIN32(GetLastError());
+            logerr(L"ReadFile failed, hr: %x", hr);
+            goto fend;
+        }
+
+        if (cbRead != cbToRead)
+        {
+            hr = E_UNEXPECTED;
+            goto fend;
+        }
+
+        cbRemaining -= cbRead;
+
+        if (!CryptHashData(hHash, (LPCBYTE)pBuffer, cbRead, 0))
+        {
+            hr = HRESULT_FROM_WIN32(GetLastError());
+            logerr(L"CryptHashData failed, hr: %x", hr);
+            goto fend;
+        }
+    }
+
+    if (!CryptGetHashParam(hHash, HP_HASHVAL, pbHash, &dwHashSize, 0))
+    {
+        hr = HRESULT_FROM_WIN32(GetLastError());
+        logerr(L"CryptGetHashParam failed, hr: %x", hr);
+        goto fend;
+    }
+
+fend:
+    if (hHash != NULL)
+    {
+        CryptDestroyHash(hHash);
+        hHash = NULL;
+    }
+    CHL_MmFree(&pBuffer);
+    return hr;
 }
